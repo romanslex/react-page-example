ebd374ba75a3f79c2b97cea535f8ed2572b68087
Roman Gasyul <romanslex@gmail.com>
2020-02-17 15:10:46 +0300

    added publishedAt

--- src/components/Articles/Article.js
+++ src/components/Articles/Article.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-export default ({imgSrc, title, description}) => {
+export default ({imgSrc, title, description, publishedAt}) => {
   return (
-      <li className="media mb-2" style={{minHeight: '80px'}}>
+      <li className="media mb-3" style={{minHeight: '80px'}}>
         <img src={imgSrc} className="mr-3" alt=""/>
         <div className="media-body">
           <h5 className="mt-0 mb-1">{title}</h5>
-          <p>{description}</p>
+          <div className="h6">Дата публикации: <small>{publishedAt}</small></div>
+          <div>{description}</div>
         </div>
       </li>
   );
